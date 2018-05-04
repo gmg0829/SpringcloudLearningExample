ba06d0c1ada57eae07aab0de5a6eadf09e092c18
gmg <minggang0829@gmail.com>
2018-05-04 17:07:26 +0800

    研究ribbon负载均衡源码

--- spring-cloud-ribbon/src/main/java/com/gmg/AppClient.java
+++ spring-cloud-ribbon/src/main/java/com/gmg/AppClient.java
@@ -1,5 +1,6 @@
 package com.gmg;
 
+import com.netflix.loadbalancer.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -37,4 +38,17 @@ public class AppClient
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+
+	//AbstractLoadBalancer
+	// BaseLoadBalancer  choseServer //挑选一个具体的微服务实例  addServer markServerDown
+	// getReachableServers//获取可用的微服务实例  getAllServers 获取所有的微服务实例
+	// DynamicServerListLoadBalancer 对BaseLoadBalancer的拓展
+	// ZoneAffinityServerListFilter 区域感知的方式实现服务的过滤
+	// AbstractLoadBalancerRule 负载均衡策略类
+	// RandomRule 随机选择一个服务实例  在全部的服务实例个数中获取一个随机数，在拿这个随机数获取可用的服务
+	// RoundRobinRule 先行轮询的方式选择一个实例  如果一直选择不到sever超过10次，那么就会尝试结束
+     // RetryRule 重试机制的实例选择功能 默认使用了RoundRobinRule实例，选择到返回，若选择不到根据设置的阈值（maxRetryMillis[500]+当前时间戳），当超过当前阈值，则返回null。
+	// WeightedResponseTimeRule 更具权重挑选实例  权重=总的响应时间-实例的平均响应时间    平均时间越短 权重区间越大，被选到的概率越大
+	//生成一个[0,最大权重值]的随机数，如果当前实例的权重值大于随机数，拿当前实例的索引值去获取当前实例
+
 }
