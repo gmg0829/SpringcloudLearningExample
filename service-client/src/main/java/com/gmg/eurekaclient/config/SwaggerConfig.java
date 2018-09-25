cb7681427648a2eed8958eb3c40b81d1298c2a61
gmg <minggang0829@gmail.com>
2018-09-25 16:29:31 +0800

    commit

---
+++ service-client/src/main/java/com/gmg/eurekaclient/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.gmg.eurekaclient.config;
+
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author xujin
+ * swagger配置
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Swagger API")
+                .description("test")
+                .termsOfServiceUrl("")
+                .contact(new Contact("gmg", "http://gmg0829.top", "minggang0829@gmail.com"))
+                .version("2.0")
+                .build();
+    }
+
+}
--- service-client/src/main/java/com/gmg/eurekaclient/controller/IndexController.java
+++ service-client/src/main/java/com/gmg/eurekaclient/controller/IndexController.java
@@ -1,5 +1,9 @@
 package com.gmg.eurekaclient.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2018/9/25  14:22
  */
 @RestController
+@Api("测试集成Swagger")
 public class IndexController {
+
+    @ApiOperation(value = "v1", notes = "v1测试")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "name",  value = "字符串name", required = true, dataType = "String")
+    })
     @GetMapping(value = "/v1")
-    public String v1() {
-        return "v1";
+    public String v1(String name) {
+        return name;
     }
 
     @GetMapping(value = "/v2")
